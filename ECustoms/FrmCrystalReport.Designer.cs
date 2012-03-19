85d5c5d8e9bbdfa9de22089c26df61e38db87320
cuongtrinhdinh87 <cuongtrinhdinh87@gmail.com>
2012-03-19 03:00:16 +0000

    - update tree group report

--- ECustoms/FrmCrystalReport.Designer.cs
+++ ECustoms/FrmCrystalReport.Designer.cs
@@ -50,7 +50,6 @@
             this.crystalReportViewer1.Name = "crystalReportViewer1";
             this.crystalReportViewer1.Size = new System.Drawing.Size(1024, 501);
             this.crystalReportViewer1.TabIndex = 0;
-            this.crystalReportViewer1.ToolPanelView = CrystalDecisions.Windows.Forms.ToolPanelViewType.None;
             // 
             // FrmCrystalReport
             // 
