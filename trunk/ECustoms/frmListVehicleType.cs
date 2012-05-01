8adf42ae14823f5aff17f5663a0bb4c586a65b29
tungdt.dtt@gmail.com <tungdt.dtt@gmail.com@999dac9f-67ae-3838-d6b5-0239dcaf1385>
2012-05-01 16:38:21 +0000

    Issue 83:[QLTP] thêm bảng thông tin về loại trọng tải phương tiện: add form

---
+++ trunk/ECustoms/frmListVehicleType.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using ECustoms.BOL;
+using ECustoms.DAL;
+using log4net;
+using ECustoms.Utilities;
+using CrystalDecisions.CrystalReports.Engine;
+using CrystalDecisions.Shared;
+
+namespace ECustoms
+{
+    public partial class frmListVehicleType : Form
+    {
+        private UserInfo userinfo;
+        public frmListVehicleType()
+        {
+            InitializeComponent();
+        }
+
+        public frmListVehicleType(UserInfo _userInfo)
+        {
+            InitializeComponent();
+            userinfo = _userInfo;
+
+        }
+
+        private void frmListVehicleType_Load(object sender, EventArgs e)
+        {
+            this.Text = "Danh sách loại phương tiện và biểu phí" + ConstantInfo.MESSAGE_TITLE;
+        }
+    }
+}
