6cca6cad545fafb1fbe088f611bab1a05f8a89b8
tungdt.dtt@gmail.com <tungdt.dtt@gmail.com@999dac9f-67ae-3838-d6b5-0239dcaf1385>
2012-05-02 16:52:56 +0000

    update UI

--- trunk/ECustoms/frmListVehicleType.Designer.cs
+++ trunk/ECustoms/frmListVehicleType.Designer.cs
@@ -192,13 +192,14 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(817, 483);
+            this.ClientSize = new System.Drawing.Size(796, 445);
             this.Controls.Add(this.btnClose);
             this.Controls.Add(this.btnDelete);
             this.Controls.Add(this.grbUser);
             this.Controls.Add(this.btnAdd);
             this.Controls.Add(this.btnUpdate);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "frmListVehicleType";
             this.Load += new System.EventHandler(this.frmListVehicleType_Load);
             this.grbUser.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.grvVehicleType)).EndInit();
