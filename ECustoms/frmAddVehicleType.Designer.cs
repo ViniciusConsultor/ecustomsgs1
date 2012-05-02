89cfadd337007993619f83683b6e7eadbe2110e9
tungdt.dtt <tungdt.dtt@gmail.com>
2012-05-02 16:52:56 +0000

    update UI

--- ECustoms/frmAddVehicleType.Designer.cs
+++ ECustoms/frmAddVehicleType.Designer.cs
@@ -206,7 +206,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(628, 353);
+            this.ClientSize = new System.Drawing.Size(628, 347);
             this.Controls.Add(this.btnClose);
             this.Controls.Add(this.btnUpdate);
             this.Controls.Add(this.btnAdd);
