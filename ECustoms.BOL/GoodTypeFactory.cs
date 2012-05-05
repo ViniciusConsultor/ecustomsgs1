27894c35b3d70a3975e0891a1e1f744996b89725
tungdt.dtt <tungdt.dtt@gmail.com>
2012-05-05 16:09:48 +0000

    Issue 85: [QLTP] thêm dropdownlist Loại Hàng

---
+++ ECustoms.BOL/GoodTypeFactory.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using ECustoms.DAL;
+using ECustoms.Utilities;
+
+namespace ECustoms.BOL
+{
+    public class GoodTypeFactory
+    {
+        public static List<tblGoodType> SelectAll()
+        {
+            var db = new dbEcustomEntities(Common.Decrypt(ConfigurationManager.ConnectionStrings["dbEcustomEntities"].ConnectionString, true));
+            List<tblGoodType> list = db.tblGoodTypes.ToList();
+            db.Connection.Close();
+            return list;
+        }
+    }
+}
