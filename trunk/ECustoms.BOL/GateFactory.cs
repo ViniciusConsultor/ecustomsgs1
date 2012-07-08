555deca7d91a05d7836f464f7fffb7bcb66fac6b
tungdt.dtt@gmail.com <tungdt.dtt@gmail.com@999dac9f-67ae-3838-d6b5-0239dcaf1385>
2012-07-08 16:27:09 +0000

    Issue 124: [GS1] Cấu hình danh mục cửa khẩu Add tblGate

---
+++ trunk/ECustoms.BOL/GateFactory.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using ECustoms.DAL;
+using ECustoms.Utilities;
+using System.Configuration;
+using System.Data;
+
+namespace ECustoms.BOL
+{
+    public class GateFactory
+    {
+
+        public static List<tblGate> getAll()
+        {
+            dbEcustomEntities _db = new dbEcustomEntities(Common.Decrypt(ConfigurationManager.ConnectionStrings["dbEcustomEntities"].ConnectionString, true));
+            try
+            {
+                if (_db.Connection.State == ConnectionState.Closed) _db.Connection.Open();
+                return _db.tblGates.ToList();
+            }
+            catch
+            {
+                return null;
+            }
+            finally
+            {
+                _db.Connection.Close();
+            }
+        }
+
+
+        public static tblGate FindByCode(string code)
+        {
+            dbEcustomEntities _db = new dbEcustomEntities(Common.Decrypt(ConfigurationManager.ConnectionStrings["dbEcustomEntities"].ConnectionString, true));
+            try
+            {
+                if (_db.Connection.State == ConnectionState.Closed) _db.Connection.Open();
+                return _db.tblGates.Where(g => g.GateCode == code).FirstOrDefault();
+            }
+            catch
+            {
+                return null;
+            }
+            finally
+            {
+                _db.Connection.Close();
+            }
+
+        }
+
+        public static int Insert(tblGate gate)
+        {
+            dbEcustomEntities _db = new dbEcustomEntities(Common.Decrypt(ConfigurationManager.ConnectionStrings["dbEcustomEntities"].ConnectionString, true));
+            _db.Connection.Open();
+            gate.CreatedDate = CommonFactory.GetCurrentDate();
+            gate.ModifiedDate = CommonFactory.GetCurrentDate();
+            _db.AddTotblGates(gate);
+            try
+            {
+                if (_db.Connection.State == ConnectionState.Closed) _db.Connection.Open();
+                return _db.SaveChanges();
+            }
+            catch (Exception ex)
+            {
+                return -1;
+            }
+            finally
+            {
+                _db.Connection.Close();
+            }
+        }
+
+        public static int Update(tblGate gateObj)
+        {
+            dbEcustomEntities _db = new dbEcustomEntities(Common.Decrypt(ConfigurationManager.ConnectionStrings["dbEcustomEntities"].ConnectionString, true));
+            _db.Connection.Open();
+
+            tblGate originGate = _db.tblGates.Where(g => g.GateCode == gateObj.GateCode).FirstOrDefault();
+
+            if (originGate == null)
+            {
+                return -1;
+            }
+
+            originGate.GateType = gateObj.GateType;
+            originGate.GateName = gateObj.GateName;
+            originGate.Description = gateObj.Description;
+            originGate.ModifiedBy = gateObj.ModifiedBy;
+            originGate.ModifiedDate = CommonFactory.GetCurrentDate();
+
+            try
+            {
+                return _db.SaveChanges();
+            }
+            catch (Exception ex)
+            {
+                return -1;
+            }
+            finally
+            {
+                _db.Connection.Close();
+            }
+        }
+
+        public static int Delete(String gateCode)
+        {
+            dbEcustomEntities _db = new dbEcustomEntities(Common.Decrypt(ConfigurationManager.ConnectionStrings["dbEcustomEntities"].ConnectionString, true));
+            try
+            {
+                if (_db.Connection.State == ConnectionState.Closed) _db.Connection.Open();
+                var gate = _db.tblGates.FirstOrDefault(vt => vt.GateCode == gateCode);
+                _db.DeleteObject(gate);
+                return _db.SaveChanges();
+            }
+            catch
+            {
+                return -1;
+            }
+            finally
+            {
+                _db.Connection.Close();
+            }
+        }
+    }
+}
