fadab1882ab45a1618d38aa35585f9004919a57b
thanhhuyit@gmail.com <thanhhuyit@gmail.com@999dac9f-67ae-3838-d6b5-0239dcaf1385>
2012-07-31 18:27:45 +0000

    TechLink.DatabaseViewer, techlink.tools

---
+++ trunk/TechLink.Tools/TechLink.DatabaseViewer/UpgradeDatabase.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using TechLink.DatabaseViewer.Commands;
+using TechLink.DatabaseViewer.DataAccess;
+
+namespace TechLink.DatabaseViewer
+{
+    public class UpgradeDatabase
+    {
+        public static string[] CommandNames = new string[] { "1.0.1" };
+
+        public Dictionary<string, UpgradeCommandBase> UpgradeCommands =new Dictionary<string, UpgradeCommandBase>();
+        public void InitialCommands(SqlCopier sqlCopier)
+        {
+            UpgradeCommandBase cmd1 = new UpgradeVersion100(sqlCopier);
+            UpgradeCommands.Add(CommandNames[0], cmd1);
+        }
+    }
+}
