c14aa54638883f0fc7cb0864d0eaf7e291442042
thanhhuyit <thanhhuyit@gmail.com>
2012-08-01 15:16:48 +0000

    sync service

---
+++ TechLink.NetRemoting.SyncService/AbstractServerConsole/IServerConsole.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AbstractServerConsole
+{
+    public interface IServerConsole
+    {
+        void Start(string[] args);
+        bool Started { get; }
+        void Stop();
+        string ServerPort { get; }
+        void CleanUpBeforeClosed();
+    }
+}
