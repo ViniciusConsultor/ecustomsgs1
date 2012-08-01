c124473a05fba760a01690c210647509dbb6998b
thanhhuyit <thanhhuyit@gmail.com>
2012-08-01 17:37:19 +0000

    sync service

---
+++ TechLink.NetRemoting.SyncService/ServerPrevalence/SQLGenericServer.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using ECustoms.DAL;
+using GenericRemoteServer.General;
+
+namespace ServerPrevalence
+{
+    public class SQLGenericServer : GenericServer
+    {
+        public SQLGenericServer(IDataController database)
+            : base(database)
+        {
+        }
+
+        #region Overrides of GenericServer
+
+        public override void Close()
+        {
+            //DataController.Disconnect();
+        }
+
+        public override void InitSystemOptions()
+        {
+            //List<MedicineItem> medicineItems = DataController.GetAllMedicineItems();
+        }
+
+        #endregion
+    }
+}
