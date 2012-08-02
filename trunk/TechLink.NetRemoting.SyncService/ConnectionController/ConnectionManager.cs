053f62bd76cb677854a4a03a75f4c756be633dc5
thanhhuyit@gmail.com <thanhhuyit@gmail.com@999dac9f-67ae-3838-d6b5-0239dcaf1385>
2012-08-02 00:42:16 +0000

    sync service

---
+++ trunk/TechLink.NetRemoting.SyncService/ConnectionController/ConnectionManager.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using ExceptionHandler;
+using ExceptionHandler.Logging;
+
+namespace ConnectionController
+{
+    public class ConnectionManager
+    {
+        public static ServerInterfacesHelper ConnectToServer(string serverURL, string serverPORT, string applicationHandle, string clientPort)
+        {
+            ConnectionBuilder connectionBuilder = null;
+            try
+            {
+                connectionBuilder = new ConnectionBuilder(serverURL, serverPORT, applicationHandle, clientPort);
+                connectionBuilder.Connect();
+                TestServerConnection(connectionBuilder);
+            }
+            catch (Exception e)
+            {
+                //ProcessException.Handle(e);
+                //Console.Write(e);
+                new WindowsServiceLog().WriteEntry(e, "ServerInterfacesHelper::ConnectToServer");
+                return null;
+            }
+            ServerInterfacesDispatcher serverInterfacesDispatcher = new ServerInterfacesDispatcher(connectionBuilder.RemoteServer, connectionBuilder.ProxyRemoteServer);
+            return new ServerInterfacesHelper(serverInterfacesDispatcher);
+        }
+
+        private static void TestServerConnection(ConnectionBuilder connectionBuilder)
+        {
+            //any server opecation call will do
+            //bool securityEnabled = connectionBuilder.RemoteServer.SecurityEnabled;
+            //connectionBuilder.RemoteServer.SyncUserInfo();
+        }
+
+    }
+}
