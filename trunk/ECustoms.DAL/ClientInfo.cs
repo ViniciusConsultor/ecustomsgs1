a0261232c8645546185bbe5f175d8684a93f7512
thanhhuyit@gmail.com <thanhhuyit@gmail.com@999dac9f-67ae-3838-d6b5-0239dcaf1385>
2012-08-02 01:06:07 +0000

    sync service

---
+++ trunk/ECustoms.DAL/ClientInfo.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ECustoms.DAL
+{
+    public class ClientInfo
+    {
+        public string Name { get; set; }
+        public string Serial { get; set; }
+    }
+}
