761d0ca90df180962407d6978b746099ed9b3afd
cuongtrinhdinh87@gmail.com <cuongtrinhdinh87@gmail.com@999dac9f-67ae-3838-d6b5-0239dcaf1385>
2012-09-10 09:57:28 +0000

    - fixed insert BrachId from Mr Huy

---
+++ trunk/ECustoms.DAL/dbEcustomEntitiesExt.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using ApplicationUtils.Utils;
+using ECustoms.Utilities;
+
+namespace ECustoms.DAL
+{
+    public partial class dbEcustomEntities
+    {
+        public void AddObject(string tableName, object obj)
+        {
+
+            var fields = DatasourceUtils.GetAllPropertiesNameOfObject(obj);
+            if (fields.Contains("BranchId"))
+            {
+                string str = DatasourceUtils.GetStringProperty(obj, "BranchId");
+
+                if (string.IsNullOrEmpty(str))
+                {
+                    DatasourceUtils.SetValueOfProperty(obj, "BranchId", FDHelper.RgCodeOfUnit());
+                }
+            }
+
+            base.AddObject(tableName, obj);
+        }
+    }
+}
