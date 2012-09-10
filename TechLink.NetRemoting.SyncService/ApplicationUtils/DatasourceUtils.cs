39d44d863ddb4dd76792689701f83a1565f4f1d4
cuongtrinhdinh87 <cuongtrinhdinh87@gmail.com>
2012-09-10 09:57:28 +0000

    - fixed insert BrachId from Mr Huy

--- TechLink.NetRemoting.SyncService/ApplicationUtils/DatasourceUtils.cs
+++ TechLink.NetRemoting.SyncService/ApplicationUtils/DatasourceUtils.cs
@@ -51,12 +51,77 @@ namespace ApplicationUtils.Utils
         if (objProp == null)
           return null;
 
-        return (objProp.GetValue(o, null)).ToString();
+          var returnValue = (objProp.GetValue(o, null));
+          return returnValue == null ? string.Empty : returnValue.ToString();
       }
       catch
       {
         return null;
       }
+    }
+
+    public static bool SetValueOfProperty(object o, string property, object value)
+    {
+        try
+        {
+            if (o is DataRowView)
+            {
+                DataRowView drv = (DataRowView)o;
+                drv[property] = value;
+            }
+
+            PropertyInfo objProp = o.GetType().GetProperty(property);
+            objProp.SetValue(o, value, null);
+        }
+        catch
+        {
+            return false;
+        }
+
+        return true;
+    }
+
+    public static List<string> GetAllPropertiesNameOfObject(object getObject)
+    {
+        List<string> cols = new List<string>();
+        if (getObject is DataTable)
+        {
+            DataTable dataTable = getObject as DataTable;
+
+            for (int i = 0; i < dataTable.Columns.Count; i++)
+            {
+                cols.Add(dataTable.Columns[i].ColumnName);
+            }
+
+            return cols;
+        }
+        else
+        {
+            //Check later
+            object newObj = null;
+            if ( getObject.GetType().IsGenericType && getObject.GetType().GetGenericTypeDefinition() == typeof(List<>))
+            {
+                IEnumerable list = getObject as IEnumerable;
+                IEnumerator tmp = list.GetEnumerator();
+                if (tmp.MoveNext())
+                    newObj = tmp.Current;
+                //IObjectData data = newObj as IObjectData;
+            }
+            else
+            {
+                newObj = getObject;
+            }
+
+            if (newObj != null)
+            {
+                PropertyInfo[] propertyInfos = newObj.GetType().GetProperties();
+                foreach (PropertyInfo propertyInfo in propertyInfos)
+                {
+                    cols.Add(propertyInfo.Name);
+                }
+            }
+        }
+        return cols;
     }
     #endregion
   }
