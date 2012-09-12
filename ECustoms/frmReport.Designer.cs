b686eabf8fc9434f6501b5e00ae4006d6a965eb1
tungdt.dtt <tungdt.dtt@gmail.com>
2012-09-12 17:37:43 +0000

    Issue 141: Chỉnh sửa báo cáo ( them dieu kien tim theo loai hinh)

--- ECustoms/frmReport.Designer.cs
+++ ECustoms/frmReport.Designer.cs
@@ -31,10 +31,12 @@
             this.components = new System.ComponentModel.Container();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmReport));
             this.groupBox1 = new System.Windows.Forms.GroupBox();
-            this.label1 = new System.Windows.Forms.Label();
-            this.cbUnit = new System.Windows.Forms.ComboBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.cbType = new System.Windows.Forms.ComboBox();
             this.tblReportTypeBindingSource1 = new System.Windows.Forms.BindingSource(this.components);
             this.dataSet2 = new ECustoms.DataSet2();
+            this.label1 = new System.Windows.Forms.Label();
+            this.cbUnit = new System.Windows.Forms.ComboBox();
             this.btnSearch = new System.Windows.Forms.Button();
             this.label3 = new System.Windows.Forms.Label();
             this.cbReportType = new System.Windows.Forms.ComboBox();
@@ -51,6 +53,8 @@
             // 
             // groupBox1
             // 
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.cbType);
             this.groupBox1.Controls.Add(this.label1);
             this.groupBox1.Controls.Add(this.cbUnit);
             this.groupBox1.Controls.Add(this.btnSearch);
@@ -68,6 +72,37 @@
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Điều kiện tìm kiếm";
             this.groupBox1.Enter += new System.EventHandler(this.groupBox1_Enter);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F);
+            this.label4.Location = new System.Drawing.Point(6, 64);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(53, 13);
+            this.label4.TabIndex = 25;
+            this.label4.Text = "Loại hình:";
+            // 
+            // cbType
+            // 
+            this.cbType.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cbType.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F);
+            this.cbType.FormattingEnabled = true;
+            this.cbType.Location = new System.Drawing.Point(78, 61);
+            this.cbType.Name = "cbType";
+            this.cbType.Size = new System.Drawing.Size(250, 21);
+            this.cbType.TabIndex = 24;
+            this.cbType.ValueMember = "ReportTypeID";
+            // 
+            // tblReportTypeBindingSource1
+            // 
+            this.tblReportTypeBindingSource1.DataMember = "tblReportType";
+            this.tblReportTypeBindingSource1.DataSource = this.dataSet2;
+            // 
+            // dataSet2
+            // 
+            this.dataSet2.DataSetName = "DataSet2";
+            this.dataSet2.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
             // 
             // label1
             // 
@@ -89,16 +124,6 @@
             this.cbUnit.Size = new System.Drawing.Size(250, 21);
             this.cbUnit.TabIndex = 22;
             // 
-            // tblReportTypeBindingSource1
-            // 
-            this.tblReportTypeBindingSource1.DataMember = "tblReportType";
-            this.tblReportTypeBindingSource1.DataSource = this.dataSet2;
-            // 
-            // dataSet2
-            // 
-            this.dataSet2.DataSetName = "DataSet2";
-            this.dataSet2.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
-            // 
             // btnSearch
             // 
             this.btnSearch.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
@@ -106,7 +131,7 @@
             this.btnSearch.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F);
             this.btnSearch.Image = global::ECustoms.Properties.Resources.report224x24_24_bit;
             this.btnSearch.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.btnSearch.Location = new System.Drawing.Point(221, 115);
+            this.btnSearch.Location = new System.Drawing.Point(221, 145);
             this.btnSearch.Name = "btnSearch";
             this.btnSearch.Size = new System.Drawing.Size(107, 28);
             this.btnSearch.TabIndex = 21;
@@ -119,7 +144,7 @@
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F);
-            this.label3.Location = new System.Drawing.Point(6, 63);
+            this.label3.Location = new System.Drawing.Point(6, 93);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(61, 13);
             this.label3.TabIndex = 20;
@@ -132,7 +157,7 @@
             this.cbReportType.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.cbReportType.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F);
             this.cbReportType.FormattingEnabled = true;
-            this.cbReportType.Location = new System.Drawing.Point(78, 60);
+            this.cbReportType.Location = new System.Drawing.Point(78, 90);
             this.cbReportType.Name = "cbReportType";
             this.cbReportType.Size = new System.Drawing.Size(250, 21);
             this.cbReportType.TabIndex = 19;
@@ -143,7 +168,7 @@
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F);
-            this.label2.Location = new System.Drawing.Point(172, 93);
+            this.label2.Location = new System.Drawing.Point(172, 123);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(68, 13);
             this.label2.TabIndex = 18;
@@ -154,7 +179,7 @@
             this.dtpExportTo.CustomFormat = "dd/MM/yyyy";
             this.dtpExportTo.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F);
             this.dtpExportTo.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
-            this.dtpExportTo.Location = new System.Drawing.Point(246, 89);
+            this.dtpExportTo.Location = new System.Drawing.Point(246, 119);
             this.dtpExportTo.Name = "dtpExportTo";
             this.dtpExportTo.Size = new System.Drawing.Size(82, 20);
             this.dtpExportTo.TabIndex = 17;
@@ -164,7 +189,7 @@
             this.dtpExportFrom.CustomFormat = "dd/MM/yyyy";
             this.dtpExportFrom.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F);
             this.dtpExportFrom.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
-            this.dtpExportFrom.Location = new System.Drawing.Point(78, 89);
+            this.dtpExportFrom.Location = new System.Drawing.Point(78, 119);
             this.dtpExportFrom.Name = "dtpExportFrom";
             this.dtpExportFrom.Size = new System.Drawing.Size(88, 20);
             this.dtpExportFrom.TabIndex = 16;
@@ -173,7 +198,7 @@
             // 
             this.lblTime.AutoSize = true;
             this.lblTime.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F);
-            this.lblTime.Location = new System.Drawing.Point(6, 93);
+            this.lblTime.Location = new System.Drawing.Point(6, 123);
             this.lblTime.Name = "lblTime";
             this.lblTime.Size = new System.Drawing.Size(66, 13);
             this.lblTime.TabIndex = 13;
@@ -220,6 +245,8 @@
         private System.Windows.Forms.BindingSource tblReportTypeBindingSource;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.ComboBox cbUnit;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ComboBox cbType;
         
     }
 }
