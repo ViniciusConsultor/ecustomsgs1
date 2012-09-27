af23e5b7908bfd22701ac6d1139c7db9103fd764
cuongtrinhdinh87@gmail.com <cuongtrinhdinh87@gmail.com@999dac9f-67ae-3838-d6b5-0239dcaf1385>
2012-09-27 19:15:53 +0000

    - fixed issue thêm cột xe sang tải, xe TQ trong form tờ khai - fixed tạm thời performance form search phương tiện

--- trunk/ECustoms.DAL/CustomEcustomEntities.cs
+++ trunk/ECustoms.DAL/CustomEcustomEntities.cs
@@ -8,39 +8,122 @@ namespace ECustoms.DAL
 {
     public partial class ViewAllVehicleHasGood: EntityObject
     {
-        public List<VehicleNumber> ListVehicleChangeGood { get; set; }
-    }
-    public partial class ViewAllVehicle : EntityObject
-    {
-        public string VehicleChangeGoodVN
+        private string _vehicleChangeGoodVn;
+        private string _vehicleChangeGoodChinese;
+        private List<VehicleNumber> _listVehicleChangeGood;
+        public List<VehicleNumber> ListVehicleChangeGood
         {
             get
             {
-                if (StatusChangeGood == (byte)Utilities.Enums.VehicleChangeStatus.SangTaiTQ) return string.Empty;
+                if (_listVehicleChangeGood != null) return _listVehicleChangeGood;
+                if (StatusChangeGood == null) return null;
                 var db = new dbEcustomEntities(Common.Decrypt(ConfigurationManager.ConnectionStrings["dbEcustomEntities"].ConnectionString, true));
-
                 var listId = db.tblVehicleChanges.Where(x => x.VehicleFromID == this.VehicleID).Select(x => x.VehicleToID)
                                                 .Union(db.tblVehicleChanges.Where(x => x.VehicleToID == this.VehicleID).Select(x => x.VehicleFromID))
                                                 .Distinct();
-                var listPlateNumber = listId.Select(id => db.tblVehicles.Where(x => x.VehicleID == id).FirstOrDefault().PlateNumber).ToList();
-                return listPlateNumber.Aggregate(string.Empty, (current, item) => current + (item + ";")).TrimEnd(';');
+                _listVehicleChangeGood = listId.Select(id => db.tblVehicles.Where(x => x.VehicleID == id).FirstOrDefault()).ToList().Select(vehicle => new VehicleNumber(vehicle.PlateNumber, vehicle.VehicleID)).ToList();
+                return _listVehicleChangeGood;
             }
+            set
+            {
+                _listVehicleChangeGood = value;
+                if (StatusChangeGood == (byte) Utilities.Enums.VehicleChangeStatus.SangTaiVN)
+                {
+                    _vehicleChangeGoodVn = string.Empty;
+                    foreach (var vehicleNumber in _listVehicleChangeGood)
+                    {
+                        _vehicleChangeGoodVn += vehicleNumber.PlateNumber + ";";
+                    }
+                    _vehicleChangeGoodVn = _vehicleChangeGoodVn.TrimEnd(';');  
+                }
+                if (StatusChangeGood == (byte) Utilities.Enums.VehicleChangeStatus.SangTaiTQ)
+                {
+                    _vehicleChangeGoodChinese = string.Empty;
+                    foreach (var vehicleNumber in _listVehicleChangeGood)
+                    {
+                        _vehicleChangeGoodChinese += vehicleNumber.PlateNumber + ";";
+                    }
+                    _vehicleChangeGoodChinese = _vehicleChangeGoodChinese.TrimEnd(';');
+                }
+            }
+        }
 
+        public string VehicleChangeGoodVn
+        {
+            get
+            {
+                if (_vehicleChangeGoodVn != null) return _vehicleChangeGoodVn;
+                if (StatusChangeGood == (byte) Utilities.Enums.VehicleChangeStatus.SangTaiVN)
+                {
+                    _vehicleChangeGoodVn = string.Empty;
+                    foreach (var vehicleNumber in ListVehicleChangeGood)
+                    {
+                        _vehicleChangeGoodVn += vehicleNumber.PlateNumber + ";";
+                    }
+                    _vehicleChangeGoodVn = _vehicleChangeGoodVn.TrimEnd(';');
+                    return _vehicleChangeGoodVn;
+                }
+                return null;
+            }
         }
         public string VehicleChangeGoodChinese
         {
             get
             {
-                if (StatusChangeGood == (byte)Utilities.Enums.VehicleChangeStatus.SangTaiVN) return string.Empty;
-                var db = new dbEcustomEntities(Common.Decrypt(ConfigurationManager.ConnectionStrings["dbEcustomEntities"].ConnectionString, true));
-
-                var listId = db.tblVehicleChanges.Where(x => x.VehicleFromID == this.VehicleID).Select(x => x.VehicleToID)
-                                                .Union(db.tblVehicleChanges.Where(x => x.VehicleToID == this.VehicleID).Select(x => x.VehicleFromID))
-                                                .Distinct();
-                var listPlateNumber = listId.Select(id => db.tblVehicles.Where(x => x.VehicleID == id).FirstOrDefault().PlateNumber).ToList();
-                return listPlateNumber.Aggregate(string.Empty, (current, item) => current + (item + ";")).TrimEnd(';');
+                if (_vehicleChangeGoodChinese != null) return _vehicleChangeGoodChinese;
+                if (StatusChangeGood == (byte)Utilities.Enums.VehicleChangeStatus.SangTaiTQ)
+                {
+                    _vehicleChangeGoodChinese = string.Empty;
+                    foreach (var vehicleNumber in ListVehicleChangeGood)
+                    {
+                        _vehicleChangeGoodChinese += vehicleNumber.PlateNumber + ";";
+                    }
+                    _vehicleChangeGoodChinese = _vehicleChangeGoodChinese.TrimEnd(';');
+                    return _vehicleChangeGoodChinese;
+                }
+                return null;
             }
+        }
+    }
+    public partial class ViewAllVehicle : EntityObject
+    {
+        private string _vehicleChangeGoodVn;
+        private string _vehicleChangeGoodChinese;
+        public string VehicleChangeGoodVn
+        {
+            get
+            {
+                if (_vehicleChangeGoodVn != null) return _vehicleChangeGoodVn;
+                if (StatusChangeGood == (byte)Utilities.Enums.VehicleChangeStatus.SangTaiVN)
+                {
+                    var db = new dbEcustomEntities(Common.Decrypt(ConfigurationManager.ConnectionStrings["dbEcustomEntities"].ConnectionString, true));
+                    var listId = db.tblVehicleChanges.Where(x => x.VehicleFromID == this.VehicleID).Select(x => x.VehicleToID)
+                                                    .Union(db.tblVehicleChanges.Where(x => x.VehicleToID == this.VehicleID).Select(x => x.VehicleFromID))
+                                                    .Distinct();
+                    var listPlateNumber = listId.Select(id => db.tblVehicles.Where(x => x.VehicleID == id).FirstOrDefault().PlateNumber).ToList();
+                    _vehicleChangeGoodVn = listPlateNumber.Aggregate(string.Empty, (current, item) => current + (item + ";")).TrimEnd(';');    
+                }
+                return _vehicleChangeGoodVn;
+            }
+        }
+        public string VehicleChangeGoodChinese
+        {
+            get
+            {
+                if (_vehicleChangeGoodChinese != null) return _vehicleChangeGoodChinese;
+                if (StatusChangeGood == (byte)Utilities.Enums.VehicleChangeStatus.SangTaiTQ)
+                {
+                    var db = new dbEcustomEntities(Common.Decrypt(ConfigurationManager.ConnectionStrings["dbEcustomEntities"].ConnectionString, true));
 
+                    var listId = db.tblVehicleChanges.Where(x => x.VehicleFromID == this.VehicleID).Select(x => x.VehicleToID)
+                                                    .Union(db.tblVehicleChanges.Where(x => x.VehicleToID == this.VehicleID).Select(x => x.VehicleFromID))
+                                                    .Distinct();
+                    var listPlateNumber = listId.Select(id => db.tblVehicles.Where(x => x.VehicleID == id).FirstOrDefault().PlateNumber).ToList();
+                    _vehicleChangeGoodChinese = listPlateNumber.Aggregate(string.Empty, (current, item) => current + (item + ";")).TrimEnd(';');    
+                }
+                return _vehicleChangeGoodChinese;
+
+            }
         }
     }
     public partial class tblVehicle : EntityObject
