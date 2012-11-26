785098ad59ccc04c48b037d446e51d8e54ff8341
cuongtrinhdinh87@gmail.com <cuongtrinhdinh87@gmail.com@999dac9f-67ae-3838-d6b5-0239dcaf1385>
2012-11-26 01:32:16 +0000

    - update train

---
+++ trunk/ECustoms/Train/frmTrainPassengers.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using ECustoms.Utilities;
+
+namespace ECustoms.Train
+{
+    public partial class frmTrainPassengers : Form
+    {
+        public frmTrainPassengers()
+        {
+            InitializeComponent();
+        }
+
+        private void frmTrainPassengers_Load(object sender, EventArgs e)
+        {
+            this.Text = "Khai bao tau khach XNK" + ConstantInfo.MESSAGE_TITLE + GlobalInfo.CompanyName;
+        }
+    }
+}
