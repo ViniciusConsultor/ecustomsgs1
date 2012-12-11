b6d77860c51f0e649f8f35b909b5d64915055470
tungdt.dtt@gmail.com <tungdt.dtt@gmail.com@999dac9f-67ae-3838-d6b5-0239dcaf1385>
2012-12-11 15:56:18 +0000

    Bao cao xuat/nhap khau chuyen cua khau: DONE: UI TODO: export report

---
+++ trunk/ECustoms/FrmReportImportToReExport.Designer.cs
@@ -0,0 +1,232 @@
+﻿namespace ECustoms
+{
+    partial class FrmReportImportToReExport
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FrmReportImportToReExport));
+            this.rbExport = new System.Windows.Forms.RadioButton();
+            this.rbImport = new System.Windows.Forms.RadioButton();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.cbCustoms = new System.Windows.Forms.ComboBox();
+            this.cbCompany = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtDeclerationNumber = new System.Windows.Forms.TextBox();
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.dtpExportTo = new System.Windows.Forms.DateTimePicker();
+            this.dtpExportFrom = new System.Windows.Forms.DateTimePicker();
+            this.lblTime = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // rbExport
+            // 
+            this.rbExport.AutoSize = true;
+            this.rbExport.Location = new System.Drawing.Point(17, 35);
+            this.rbExport.Name = "rbExport";
+            this.rbExport.Size = new System.Drawing.Size(376, 24);
+            this.rbExport.TabIndex = 0;
+            this.rbExport.TabStop = true;
+            this.rbExport.Text = "Sổ theo dõi hàng hóa xuất khẩu chuyển cửa khẩu";
+            this.rbExport.UseVisualStyleBackColor = true;
+            // 
+            // rbImport
+            // 
+            this.rbImport.AutoSize = true;
+            this.rbImport.Location = new System.Drawing.Point(17, 65);
+            this.rbImport.Name = "rbImport";
+            this.rbImport.Size = new System.Drawing.Size(382, 24);
+            this.rbImport.TabIndex = 1;
+            this.rbImport.TabStop = true;
+            this.rbImport.Text = "Sổ theo dõi hàng hóa nhập khẩu chuyển cửa khẩu";
+            this.rbImport.UseVisualStyleBackColor = true;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.dtpExportTo);
+            this.groupBox1.Controls.Add(this.dtpExportFrom);
+            this.groupBox1.Controls.Add(this.lblTime);
+            this.groupBox1.Controls.Add(this.txtDeclerationNumber);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.cbCompany);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.cbCustoms);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.rbExport);
+            this.groupBox1.Controls.Add(this.rbImport);
+            this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(700, 294);
+            this.groupBox1.TabIndex = 2;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Điều kiện báo cáo";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 112);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(109, 20);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Nơi mở tờ khai";
+            // 
+            // cbCustoms
+            // 
+            this.cbCustoms.FormattingEnabled = true;
+            this.cbCustoms.Location = new System.Drawing.Point(170, 112);
+            this.cbCustoms.Name = "cbCustoms";
+            this.cbCustoms.Size = new System.Drawing.Size(524, 28);
+            this.cbCustoms.TabIndex = 3;
+            // 
+            // cbCompany
+            // 
+            this.cbCompany.FormattingEnabled = true;
+            this.cbCompany.Location = new System.Drawing.Point(170, 151);
+            this.cbCompany.Name = "cbCompany";
+            this.cbCompany.Size = new System.Drawing.Size(524, 28);
+            this.cbCompany.TabIndex = 5;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 151);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(109, 20);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Doanh nghiệp";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(14, 195);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(80, 20);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Số tờ khai";
+            // 
+            // txtDeclerationNumber
+            // 
+            this.txtDeclerationNumber.Location = new System.Drawing.Point(170, 195);
+            this.txtDeclerationNumber.Name = "txtDeclerationNumber";
+            this.txtDeclerationNumber.Size = new System.Drawing.Size(229, 26);
+            this.txtDeclerationNumber.TabIndex = 7;
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.btnSearch.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnSearch.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F);
+            this.btnSearch.Image = global::ECustoms.Properties.Resources.report224x24_24_bit;
+            this.btnSearch.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnSearch.Location = new System.Drawing.Point(605, 314);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(107, 28);
+            this.btnSearch.TabIndex = 26;
+            this.btnSearch.Text = "&Hiện báo cáo";
+            this.btnSearch.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btnSearch.UseVisualStyleBackColor = true;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(287, 250);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(98, 20);
+            this.label4.TabIndex = 25;
+            this.label4.Text = "Thơi gian tới:";
+            // 
+            // dtpExportTo
+            // 
+            this.dtpExportTo.CustomFormat = "dd/MM/yyyy";
+            this.dtpExportTo.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.dtpExportTo.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
+            this.dtpExportTo.Location = new System.Drawing.Point(402, 244);
+            this.dtpExportTo.Name = "dtpExportTo";
+            this.dtpExportTo.Size = new System.Drawing.Size(103, 26);
+            this.dtpExportTo.TabIndex = 24;
+            // 
+            // dtpExportFrom
+            // 
+            this.dtpExportFrom.CustomFormat = "dd/MM/yyyy";
+            this.dtpExportFrom.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.dtpExportFrom.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
+            this.dtpExportFrom.Location = new System.Drawing.Point(170, 246);
+            this.dtpExportFrom.Name = "dtpExportFrom";
+            this.dtpExportFrom.Size = new System.Drawing.Size(100, 26);
+            this.dtpExportFrom.TabIndex = 23;
+            // 
+            // lblTime
+            // 
+            this.lblTime.AutoSize = true;
+            this.lblTime.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTime.Location = new System.Drawing.Point(14, 250);
+            this.lblTime.Name = "lblTime";
+            this.lblTime.Size = new System.Drawing.Size(95, 20);
+            this.lblTime.TabIndex = 22;
+            this.lblTime.Text = "Thời gian từ:";
+            // 
+            // FrmReportImportToReExport
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(724, 354);
+            this.Controls.Add(this.btnSearch);
+            this.Controls.Add(this.groupBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "FrmReportImportToReExport";
+            this.Text = "Báo cáo xuất/nhập chuyển cửa khẩu";
+            this.Load += new System.EventHandler(this.FrmReportImportToReExport_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RadioButton rbExport;
+        private System.Windows.Forms.RadioButton rbImport;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox txtDeclerationNumber;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox cbCompany;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox cbCustoms;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.DateTimePicker dtpExportTo;
+        private System.Windows.Forms.DateTimePicker dtpExportFrom;
+        private System.Windows.Forms.Label lblTime;
+        private System.Windows.Forms.Button btnSearch;
+    }
+}
---
+++ trunk/ECustoms/FrmReportImportToReExport.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using ECustoms.Utilities;
+using ECustoms.Utilities.Enums;
+using log4net;
+using System.Collections.Generic;
+using ECustoms.BOL;
+using ECustoms.DAL;
+
+namespace ECustoms
+{
+    public partial class FrmReportImportToReExport : Form
+    {
+        private static log4net.ILog logger = LogManager.GetLogger("Ecustoms.FrmReportImportToReExport");
+        private Form _mainForm;
+        private UserInfo _userInfo;
+        private int _type;
+
+        public FrmReportImportToReExport()
+        {
+            InitializeComponent();
+        }
+
+        public FrmReportImportToReExport(Form mainFrom, UserInfo userInfo, int type)
+        {
+            _userInfo = userInfo;
+            _type = type;
+            InitializeComponent();
+            _mainForm = mainFrom;
+
+        }
+
+        private void FrmReportImportToReExport_Load(object sender, EventArgs e)
+        {
+            this.Text = "Bao cao xuat/nhap chuyen cua khau" + ConstantInfo.MESSAGE_TITLE + GlobalInfo.CompanyName;
+
+            //init cbCustoms
+            List<tblCustom> listCustom = CustomsFacory.getAll();
+            foreach (tblCustom cb in listCustom)
+            {
+                cb.CustomsName = cb.CustomsCode + " - " + cb.CustomsName;
+            }
+            listCustom = listCustom.OrderBy(g => g.CustomsCode).ToList();
+            tblCustom allObj = new tblCustom();
+            allObj.CustomsCode = "0";
+            allObj.CustomsName = "Tất cả";
+            listCustom.Insert(0, allObj);
+            cbCustoms.DataSource = listCustom;
+            cbCustoms.ValueMember = "CustomsCode";
+            cbCustoms.DisplayMember = "CustomsName";
+
+            //init cbCompany
+            List<tblCompany> listCompany = CompanyFactory.getAllCompany();
+            foreach (tblCompany cp in listCompany)
+            {
+                cp.CompanyName = cp.CompanyCode + " - " + cp.CompanyName;
+            }
+            listCompany = listCompany.OrderBy(g => g.CompanyCode).ToList();
+            tblCompany allCompanyObj = new tblCompany();
+            allCompanyObj.CompanyCode = "0";
+            allCompanyObj.CompanyName = "Tất cả";
+            listCompany.Insert(0, allCompanyObj);
+            cbCompany.DataSource = listCompany;
+            cbCompany.ValueMember = "CompanyCode";
+            cbCompany.DisplayMember = "CompanyName";
+        }
+    }
+}
